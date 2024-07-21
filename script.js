ec309b6b3e40b11e92b2ce629ec5a155a60f820e
Vishal Kumar Sain <102027742+vis123Git@users.noreply.github.com>
2024-07-21 14:34:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
-  //   write your code here
+	if(sampleObject[key]){
+		return true;
+	}else{
+		return false
+	}
 }
 
-// Do not change the code below
 const key = prompt("Enter Key.");
 alert(hasKey(key));
